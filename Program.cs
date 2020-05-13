0613510f14607886b9119b1e7f41c325f2c208ed
raydegroat <stockholmray@gmail.com>
2020-05-13 14:44:13 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Switch_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput; // Variable for containing user input
+
+            Console.WriteLine("Enter an integer between 0 and 10 "); //Ask user for input
+
+            userInput = Convert.ToInt32(Console.ReadLine()); //Get user input & convert to int
+
+            if ((userInput == 9) || (userInput == 10))
+            {
+
+            }
+
+            switch (userInput)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    Console.WriteLine("Fail");
+                    break;
+                case 1:
+                    Console.WriteLine("Fail");
+                    break;
+                case 2:
+                    Console.WriteLine("Fail");
+                    break;
+                case 3:
+                case num1
+                default:
+                    break;
+            }
+
+
+
+
+
+        }
+    }
+}
